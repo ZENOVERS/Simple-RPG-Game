4101c0ba75146f9f306fbf90bb0d9e94ba9dfe83
jisci <jisci88@gmail.com>
2022-01-24 15:09:41 +0900

    Update

---
+++ RPG/Assets/Mob_HP_Bar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mob_HP_Bar : MonoBehaviour
+{
+    Slider slHP;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slHP = GetComponent<Slider>();
+        //text = GameObject.Find("Mob1_HP").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.Enemy == "Mob1")
+        {
+            slHP.value = (float)Mob1_Move.Mob1_HP / Mob1_Move.Mob1_MAXHP;
+            GameObject.Find("Mob_HP_Text").GetComponent<Text>().text = Mob1_Move.Mob1_HP + "/" + Mob1_Move.Mob1_MAXHP;
+        }
+    }
+}
